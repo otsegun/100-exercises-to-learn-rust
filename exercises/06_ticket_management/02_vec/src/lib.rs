5dd910a1792ee06c45c0a42b56c3373217517a1c
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2025-12-16 11:58:18 +0100

    Add soltution for 06_ticket_management/02_vec

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,19 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+
+    // match n {
+    //     0 => 0,
+    //     1 => 1,
+    //     x => fibonacci(x - 1) + fibonacci(x - 2),
+    // }
+
+    let mut fibonacci_vector: Vec<u32> = vec![0, 1];
+    for i in 2..=n {
+        fibonacci_vector
+            .push(fibonacci_vector[(i - 1) as usize] + fibonacci_vector[(i - 2) as usize]);
+    }
+    fibonacci_vector[n as usize]
 }
 
 #[cfg(test)]
